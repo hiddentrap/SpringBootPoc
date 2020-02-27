9e63f7f4e7a641e967bbe188e0d05847f94480fd
git_jaejoo <git_jaejoo@jaejoo.iptimer.org>
2020-02-27 16:16:06 +0900

    initiating SpringBoot CodeBase
    스프링부트 프로젝트 개발용 코드베이스 설정

---
+++ src/test/java/infra/kdbc/SpringBootPoc/SpringBootPocApplicationTests.java
@@ -0,0 +1,13 @@
+package infra.kdbc.SpringBootPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
